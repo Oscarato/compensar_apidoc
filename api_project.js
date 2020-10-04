76d386d10970a308032f17eeaf02f74736aebcaa
Oscar Jimenez <oscarjimenez@qdata.io>
2020-10-04 00:34:19 -0500

    documentacion api

---
+++ api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "Documentación",
+  "version": "0.1.0",
+  "description": "Diseño del contrato del WS",
+  "title": "API para compensar",
+  "url": "http://localhost/compensar_api/public",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-10-04T05:30:41.506Z",
+    "url": "https://apidocjs.com",
+    "version": "0.25.0"
+  }
+});
